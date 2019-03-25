1976207b4bab3bad22fb2b64e7f7ad5db43da53d
Agnes <yhu.mit@gmail.com>
2019-03-25 19:47:24 -0400

    .

--- run.js
+++ run.js
@@ -1,17 +1,30 @@
+// var $ = require('jquery');
 var mitcourses   = require('./mitcourses_small');
 var getTitles    = require('./getTitles');
 var getWords     = require('./getWords');
 var getWordsFlat = require('./getWordsFlat');
 var getScores    = require('./getScores');
 
-var titles = getTitles(mitcourses); 
-console.log(titles);
 
-var words = getWords(titles);
-console.log(words);
 
-var wordsFlat = getWordsFlat(words);
-console.log(wordsFlat);
+var str = "Hello world!!!";
+var regexp = /(\w+)\W*/g;
+console.log(str.matchAll(regexp));
 
-var scores = getScores(wordsFlat);
-console.log(scores);
+var regexp = RegExp(/<h3>(.*?)\s*<br>/g);
+console.log(typeof mitcourses === 'string');
+var a = mitcourses.matchAll(regexp);
+console.log(a);
+// let arr = [...mitcourses.matchall(regexp)];
+// console.log(arr);
+// var titles = getTitles(mitcourses);
+// console.log(titles);
+
+// var words = getWords(titles);
+// console.log(words);
+
+// var wordsFlat = getWordsFlat(words);
+// console.log(wordsFlat);
+
+// var scores = getScores(wordsFlat);
+// console.log(scores);
