f301bb05de529a8b0c6d842eb1f81d56a6d49f7a
Agnes <yhu.mit@gmail.com>
2019-03-25 21:07:09 -0400

    .

--- getScores.js
+++ getScores.js
@@ -1,11 +1,11 @@
 
 var getScores = function(wordsFlat){
 
-	// -----------------------------------
-	//  YOUR CODE
-	//  Count the word frequency
-	//  Return object with word counts
-	// -----------------------------------	
+	var scores = {};
+	for (var i = 0; i < wordsFlat.length; i++) {
+	var word = wordsFlat[i];
+	scores[word] = scores[word] ? scores[word] + 1 : 1;
+	}
 
 	return scores;
 
